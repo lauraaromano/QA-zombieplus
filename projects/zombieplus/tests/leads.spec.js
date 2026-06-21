9bba5a10b9f7dc5db2dbd944a09d9078fb974496
lauraaromano <lauraromano2018@gmail.com>
2026-06-21 11:55:52 -0300

    primeiro commit

---
+++ projects/zombieplus/tests/leads.spec.js
@@ -0,0 +1,65 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+const { LandingPage } = require('./pages/LandingPage')
+
+test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Laura Teste', 'laura@gmail.com')
+
+  const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+
+
+  await landingPage.toastHaveText(message)
+});
+
+test('não deve cadastrar com email incorreto', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Laura Teste', 'laura.com.br')
+
+
+  await landingPage.alertHaveText('Email incorreto')
+
+});
+
+
+test('não deve cadastrar quando o nome não é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('', 'laura@gmail.com')
+
+
+ await landingPage.alertHaveText('Campo obrigatório')
+
+});
+
+test('não deve cadastrar quando o email não é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Laura Teste', '')
+
+
+  await landingPage.alertHaveText('Campo obrigatório')
+
+});
+
+test('não deve cadastrar quando nenhum campo é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('','')
+
+  // fazer array para cada validação que o sistema dá
+  await landingPage.alertHaveText(['Campo obrigatório', 'Campo obrigatório'])
+
+});
---
+++ projects/zombieplus/tests/pages/LandingPage.js
@@ -0,0 +1,40 @@
+import { expect } from '@playwright/test';
+export class LandingPage {
+
+    constructor(page) {
+        this.page = page
+    }
+
+    async visit(){
+        await this.page.goto('http://localhost:3000')
+    }
+
+    async openLeadModal(){
+        await this.page.getByRole('button', { name: 'Aperte o play... se tiver coragem' }).click()
+
+        await expect(
+            this.page.getByTestId('modal').getByRole('heading')
+        ).toHaveText('Fila de espera')
+    }
+
+    async submitLeadForm(name, email){
+        //ELEMENTO[PROP=VALUE]
+        await this.page.locator('input[name=name]').fill(name)
+          // com id
+        await this.page.locator('#email').fill(email)
+          //await page.locator('input[email=email]').fill(email)
+        await this.page.getByTestId('modal').getByText('Quero entrar na fila!').click()
+    }
+
+    async toastHaveText(message){
+
+        const toast = this.page.locator('.toast')
+
+        await expect(toast).toHaveText(message)
+        await expect(toast).toBeHidden({timeout: 5000})
+    }
+//target = alvo de validação
+    async alertHaveText(target) {
+          await expect(this.page.locator('.alert')).toHaveText('target')
+    }
+}
