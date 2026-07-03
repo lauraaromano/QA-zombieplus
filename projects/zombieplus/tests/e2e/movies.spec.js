6c3a8bb3f0aba8250161bf17de24381943dbb70c
lauraaromano <lauraromano2018@gmail.com>
2026-07-03 16:30:40 -0300

    aula 52 - 57

--- projects/zombieplus/tests/e2e/movies.spec.js
+++ projects/zombieplus/tests/e2e/movies.spec.js
@@ -2,33 +2,47 @@ const { test, expect } = require('../support')
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
+test.beforeAll(async () => {
+  await executeSQL(`DELETE FROM movies`)
+
+})
+
 test('deve poder cadastrar um novo filme', async ({ page }) => {
 
   const movie = data.create
-  console.log(movie.title)
   await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
 
-  //é importante estar logado
   await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
-
-  await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
-
+  await page.movies.create(movie)
   await page.toast.containText('Cadastro realizado com sucesso!')
+
+})
+
+test('não deve cadastrar quando o título é duplicado', async ({ page }) => {
+
+  const movie = data.duplicate
+
+  await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+  await page.movies.create(movie)
+  await page.toast.containText('Cadastro realizado com sucesso!')
+
+  await page.movies.create(movie)
+  await page.toast.containText('Este conteúdo já encontra-se cadastrado no catálogo')
 
 })
 
 test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {
 
-    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+  await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
 
-    await page.movies.goForm()
-    await page.movies.submit()
+  await page.movies.goForm()
+  await page.movies.submit()
 
-    await page.movies.alertHaveText([
-      'Por favor, informe o título.',
-      'Por favor, informe a sinopse.',
-      'Por favor, informe a empresa distribuidora.',
-      'Por favor, informe o ano de lançamento.'
-    ])
+  await page.movies.alertHaveText([
+    'Por favor, informe o título.',
+    'Por favor, informe a sinopse.',
+    'Por favor, informe a empresa distribuidora.',
+    'Por favor, informe o ano de lançamento.'
+  ])
 
 })
--- projects/zombieplus/tests/support/actions/Movies.js
+++ projects/zombieplus/tests/support/actions/Movies.js
@@ -14,25 +14,34 @@ export class Movies {
         await this.page.getByRole('button', {name: "Cadastrar"}).click()
     }
 
-    async create(title, overview, company, release_year){
+    async create(movie){
         await this.page.movies.goForm()
 
-        await this.page.getByLabel('Titulo do filme').fill(title)
-        await this.page.getByLabel('Sinopse').fill(overview)
+        await this.page.getByLabel('Titulo do filme').fill(movie.title)
+        await this.page.getByLabel('Sinopse').fill(movie.overview)
 
         await this.page.locator('#select_company_id .react-select__dropdown-indicator')
             .click()
 
         await this.page.locator('.react-select__option')
-            .filter({hasText: company})
+            .filter({hasText: movie.company})
             .click()
 
         await this.page.locator('#select_year .react-select__indicator')
             .click()
 
         await this.page.locator('.react-select__option')
-            .filter({hasText: release_year})
+            .filter({hasText: movie.release_year})
             .click()
+
+        await this.page
+            .locator('input[name=cover]')
+            .setInputFiles('tests/support/fixtures' + movie.cover)
+// se o featured for true acontece , se não, passa 
+        if (movie.featured) {
+            await this.page.locator('.featured .react-switch').click()
+        }
+
 
         await this.page.movies.submit()
     } 
